9c0914df201c638aedccfe7373d38b8abb19deea
Juano <juanjosemunozva@gmail.com>
2024-08-16 05:38:53 -0500

    subí el cors a las redes

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const {connection} = require('./database/connection');
-
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 4000;
+app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'));
 
