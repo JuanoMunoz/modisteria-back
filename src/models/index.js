4f14c7121115924be3ee62afb0d993a9c00c6e0a
yarce11 <camiloyarce11@gmail.com>
2024-09-06 13:01:12 -0500

    objetivo en citas

---
+++ src/models/index.js
@@ -0,0 +1,10 @@
+const { Usuario } = require("./usuario.model");
+const { Role } = require("./role.model");
+const { Permiso } = require("./permiso.model");
+const { Insumo } = require("./insumo.model");
+const { Categoria } = require("./categoria.model");
+const { Catalogo } = require('./catalogo.model.js');
+const { Prenda } = require('./prenda.model.js');
+const { Cita } = require('./cita.model.js');
+
+module.exports = { Usuario, Role, Permiso, Insumo, Categoria, Catalogo, Prenda, Cita };
