d5a061635331b5390e14f6f38a195779db613b68
Estefania <estefaniadiaz1110@gmail.com>
2024-09-24 21:06:13 -0500

    imagen en cita

--- src/routers/cita.routes.js
+++ src/routers/cita.routes.js
@@ -3,15 +3,17 @@ const { Router } = require("express");
 const { getAllCitas, getCitaById, createCita, updateCita, deleteCita, statusCita } = require("../controllers/cita.controller");
 const { verifyToken } = require("../utils/verifyToken");
 const { validateRolPermisoCita } = require("../validators/validations.validator");
+const { upload } = require('../utils/image.js');
+
 const router = Router();
 
 router.get('/getAllCitas', [verifyToken, validateRolPermisoCita], getAllCitas);
 
 router.get('/getCitaById/:id', [verifyToken, validateRolPermisoCita], getCitaById);
 
-router.post('/createCita', [verifyToken, validateRolPermisoCita], createCita);
+router.post('/createCita', [verifyToken, validateRolPermisoCita, upload.single('file')], createCita);
 
-router.put('/updateCita/:id', [verifyToken, validateRolPermisoCita], updateCita);
+router.put('/updateCita/:id', [verifyToken, validateRolPermisoCita, upload.single('file')], updateCita);
 
 router.put('/statusCita/:id', [verifyToken, validateRolPermisoCita], statusCita);
 
