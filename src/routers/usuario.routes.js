6c5997afd77dc06a785b4841c1f16e7e52d69e03
Juano <juanjosemunozva@gmail.com>
2024-10-18 07:22:31 -0500

    ulalalala

--- src/routers/usuario.routes.js
+++ src/routers/usuario.routes.js
@@ -1,35 +1,78 @@
 const { Router } = require("express");
-const { getAllUsers, getUserById, createUser, updateUser, statusUser, deleteUser, login, forgotPassword, resetPassword, getCodePassword, getCodeVerification, verifyUser, isYourCurrentPassword, resetCurrentPassword, updateInfo, createUsuario} = require("../controllers/usuario.controller");
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  statusUser,
+  deleteUser,
+  login,
+  forgotPassword,
+  resetPassword,
+  getCodePassword,
+  getCodeVerification,
+  verifyUser,
+  isYourCurrentPassword,
+  resetCurrentPassword,
+  updateInfo,
+  createUsuario,
+} = require("../controllers/usuario.controller");
 const { verifyToken } = require("../utils/verifyToken");
-const { validateRolPermisoUsuario } = require("../validators/validations.validator");
+const {
+  validateRolPermisoUsuario,
+} = require("../validators/validations.validator");
 const router = Router();
 
-router.get('/getAllUsers', [verifyToken, validateRolPermisoUsuario], getAllUsers);
+router.get(
+  "/getAllUsers",
+  [verifyToken, validateRolPermisoUsuario],
+  getAllUsers
+);
 
-router.get('/getUserById/:id', [verifyToken, validateRolPermisoUsuario], getUserById);
+router.get(
+  "/getUserById/:id",
+  [verifyToken, validateRolPermisoUsuario],
+  getUserById
+);
 
-router.post('/createUser', [], createUser);
-router.post('/createUsuario', [], createUsuario);
+router.post("/createUser", [], createUser);
+router.post(
+  "/createUsuario",
+  [verifyToken, validateRolPermisoUsuario],
+  createUsuario
+);
 
-router.post('/getCodeVerification', [], getCodeVerification);
+router.post("/getCodeVerification", [], getCodeVerification);
 
-router.post('/verifyUser', [], verifyUser);
+router.post("/verifyUser", [], verifyUser);
 
-router.put('/updateUser/:id', [verifyToken, validateRolPermisoUsuario], updateUser);
-router.put('/updateInfo/:id', [verifyToken], updateInfo);
+router.put(
+  "/updateUser/:id",
+  [verifyToken, validateRolPermisoUsuario],
+  updateUser
+);
+router.put("/updateInfo/:id", [verifyToken], updateInfo);
 
-router.put('/statusUser/:id', [verifyToken, validateRolPermisoUsuario], statusUser);
+router.put(
+  "/statusUser/:id",
+  [verifyToken, validateRolPermisoUsuario],
+  statusUser
+);
 
-router.delete('/deleteUser/:id', [verifyToken, validateRolPermisoUsuario], deleteUser); 
+router.delete(
+  "/deleteUser/:id",
+  [verifyToken, validateRolPermisoUsuario],
+  deleteUser
+);
 
-router.post('/getCodePass', [], getCodePassword)
+router.post("/getCodePass", [], getCodePassword);
 
-router.post('/login', [], login);
+router.post("/login", [], login);
 
-router.post('/forgotPassword', [], forgotPassword)
+router.post("/forgotPassword", [], forgotPassword);
 
-router.post('/resetPassword', [], resetPassword)
-router.post('/isYourCurrentPassword', [], isYourCurrentPassword)
-router.post('/resetCurrentPassword', [verifyToken], resetCurrentPassword)
+router.post("/resetPassword", [], resetPassword);
+router.post("/isYourCurrentPassword", [], isYourCurrentPassword);
+router.post("/resetCurrentPassword", [verifyToken], resetCurrentPassword);
 
 module.exports = router;
