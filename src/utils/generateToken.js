f37455bd8011d72bf8adb8bf569d6f34c15b310f
Juano <juanjosemunozva@gmail.com>
2024-10-20 20:44:32 -0500

    te amo lesley gore

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,15 +1,18 @@
-const jwt = require('jsonwebtoken');
-const { use } = require('./mailer');
+const jwt = require("jsonwebtoken");
+const { use } = require("./mailer");
 exports.generateToken = (user) => {
-    payload = {
-        id: user.id,
-        email: user.email,
-        nombre: user.nombre,
-        telefono: user.telefono,
-        password: user.password,
-        direccion: user.direccion,
-        roleId: user.roleId
-    }
-    const token = jwt.sign({ payload }, process.env.KEY_JWT , { expiresIn: '20h' });
-    return token;
-}
+  payload = {
+    id: user.id,
+    email: user.email,
+    nombre: user.nombre,
+    telefono: user.telefono,
+    password: user.password,
+    direccion: user.direccion,
+    roleId: user.roleId,
+    role: user.role,
+  };
+  const token = jwt.sign({ payload }, process.env.KEY_JWT, {
+    expiresIn: "20h",
+  });
+  return token;
+};
