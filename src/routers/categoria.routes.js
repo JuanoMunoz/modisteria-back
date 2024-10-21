27634a77c557fd12685291f83ef530c4ef3362bc
Juano <juanjosemunozva@gmail.com>
2024-10-20 20:29:33 -0500

    g

--- src/routers/categoria.routes.js
+++ src/routers/categoria.routes.js
@@ -1,21 +1,53 @@
 const { Router } = require("express");
-const { getAllCategorias, getCategoriaById, createCategoria, updateCategoria, deleteCategoria, statusCategoria, getCategoriaByTipo} = require("../controllers/categoria.controller");
+const {
+  getAllCategorias,
+  getCategoriaById,
+  createCategoria,
+  updateCategoria,
+  deleteCategoria,
+  statusCategoria,
+  getCategoriaByTipo,
+} = require("../controllers/categoria.controller");
 const { verifyToken } = require("../utils/verifyToken");
 const { buscarPermiso } = require("../validators/validations.validator");
 const router = Router();
 
-router.get('/getAllCategorias', [verifyToken, buscarPermiso('CATEGORIA')], getAllCategorias);
+router.get("/getAllCategorias", getAllCategorias);
 
-router.get('/getCategoriaById/:id', [verifyToken, buscarPermiso('CATEGORIA')], getCategoriaById);
+router.get(
+  "/getCategoriaById/:id",
+  [verifyToken, buscarPermiso("CATEGORIA")],
+  getCategoriaById
+);
 
-router.get('/getCategoriaByTipo/:tipo', [verifyToken, buscarPermiso('CATEGORIA')], getCategoriaByTipo)
+router.get(
+  "/getCategoriaByTipo/:tipo",
+  [verifyToken, buscarPermiso("CATEGORIA")],
+  getCategoriaByTipo
+);
 
-router.post('/createCategoria', [verifyToken, buscarPermiso('CATEGORIA')], createCategoria);
+router.post(
+  "/createCategoria",
+  [verifyToken, buscarPermiso("CATEGORIA")],
+  createCategoria
+);
 
-router.put('/updateCategoria/:id', [verifyToken, buscarPermiso('CATEGORIA')], updateCategoria);
+router.put(
+  "/updateCategoria/:id",
+  [verifyToken, buscarPermiso("CATEGORIA")],
+  updateCategoria
+);
 
-router.put('/statusCategoria/:id', [verifyToken, buscarPermiso('CATEGORIA')], statusCategoria);
+router.put(
+  "/statusCategoria/:id",
+  [verifyToken, buscarPermiso("CATEGORIA")],
+  statusCategoria
+);
 
-router.delete('/deleteCategoria/:id', [verifyToken, buscarPermiso('CATEGORIA')], deleteCategoria);
+router.delete(
+  "/deleteCategoria/:id",
+  [verifyToken, buscarPermiso("CATEGORIA")],
+  deleteCategoria
+);
 
 module.exports = router;
