689e84bf5228d2852ed419136d32bb986cbf2c0c
Juano <juanjosemunozva@gmail.com>
2024-10-23 05:51:25 -0500

    include permisos en rol

--- src/repositories/role.repository.js
+++ src/repositories/role.repository.js
@@ -1,44 +1,46 @@
 const { Role, RolesPermisos, Permiso, Estado } = require("../models");
 
 exports.getAllRoles = async () => {
-    return await Role.findAll();
+  return await Role.findAll({ include: { model: Permiso } });
 };
 
 exports.getRoleById = async (id) => {
-    return await Role.findOne({ 
-        where: { id }
-        // include: [ { model: RolesPermisos, as: "roles_permisos"} ], 
-        //raw: true Devuelve el objeto literal desde la bd
-    });
-}
+  return await Role.findOne({
+    where: { id },
+    // include: [ { model: RolesPermisos, as: "roles_permisos"} ],
+    //raw: true Devuelve el objeto literal desde la bd
+  });
+};
 
 exports.createRole = async (role) => {
-    return await Role.create(role);
-}
+  return await Role.create(role);
+};
 
 exports.updateRole = async (id, nombre, permisosId, estadoId) => {
-    return await Role.update({nombre, permisosId, estadoId}, { where: { id } });
-}
+  return await Role.update({ nombre, permisosId, estadoId }, { where: { id } });
+};
 
 exports.statusRole = async (id) => {
-    return await Role.update({ estado: false }, { where: { id } });
-}
+  return await Role.update({ estado: false }, { where: { id } });
+};
 
 exports.deleteRole = async (id) => {
-    const role = await Role.findOne({
-        where: { id }
-    });
-    
-    if (!role) {
-        throw new Error("Rol no encontrado");
-    }
+  const role = await Role.findOne({
+    where: { id },
+  });
 
-    const existePermiso = role.permisosId && role.permisosId.length > 0;
-    const existeEstado = await Estado.findOne({ where: { id: role.estadoId } });
-    
-    if (existePermiso || existeEstado) {
-        throw new Error("No se puede eliminar el rol porque está asociado a registros en otras tablas");
-    }
-    
-    return await Role.destroy({ where: { id } });
+  if (!role) {
+    throw new Error("Rol no encontrado");
+  }
+
+  const existePermiso = role.permisosId && role.permisosId.length > 0;
+  const existeEstado = await Estado.findOne({ where: { id: role.estadoId } });
+
+  if (existePermiso || existeEstado) {
+    throw new Error(
+      "No se puede eliminar el rol porque está asociado a registros en otras tablas"
+    );
+  }
+
+  return await Role.destroy({ where: { id } });
 };
