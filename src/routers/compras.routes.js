5ab36065d8aac458983701e35c7d649e342949fd
yarce11 <camiloyarce11@gmail.com>
2024-11-27 18:41:36 -0500

    proveedor

---
+++ src/routers/compras.routes.js
@@ -0,0 +1,20 @@
+/* const { Router } = require("express");
+const { getAllCompras, getCompraById, createCompra, updateCompra, deleteCompra } = require("../controllers/proveedor.controller");
+const { verifyToken } = require("../utils/verifyToken");
+const { buscarPermiso } = require("../validators/validations.validator");
+const router = Router();
+
+router.get("/getAllCompras", getAllCompras);
+
+router.get("/getCompraById/:id", getCompraById);
+
+router.post("/createCompra", createCompra);
+
+router.put("/updateCompra/:id", updateCompra);
+
+router.put("/statusCompra/:id", statusCompra);
+
+router.delete("/deleteCompra/:id", deleteCompra);
+
+module.exports = router;
+ */
